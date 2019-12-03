301f800994a01e0104b37c91b1e7b0023745b33b
TargaryenQi <44879154+TargaryenQi@users.noreply.github.com>
2019-12-03 16:52:26 -0500

    adapt brute force to DBSearch interface

--- src/main/java/edu/bu/cs622/db/BruteForce.java
+++ src/main/java/edu/bu/cs622/db/BruteForce.java
@@ -2,13 +2,78 @@ package edu.bu.cs622.db;
 
 import edu.bu.cs622.message.SearchResult;
 import edu.bu.cs622.message.SearchType;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class BruteForce {
+public class BruteForce implements DBSearch{
+
+  @Override
+  public int howManyStepsOnOneDay(String date) {
+    int count = 0;
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader("data/Activity.data"));
+      String line = null;
+      line = reader.readLine();
+      while (line != null) {
+        if(line.contains(date))  {
+          JSONObject jsonObject = new JSONObject(line);
+          int step_delta = jsonObject.getJSONObject("sensor_data").getInt("step_delta");
+          count += step_delta;
+        }
+        line = reader.readLine();
+      }
+
+      reader.close();
+    } catch ( IOException e) {
+      e.printStackTrace();
+    }
+    return count;
+  }
+
+  @Override
+  public boolean AreThereRunningEvent(String date) {
+    boolean result = false;
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader("data/ActivityFit.data"));
+      String line = null;
+      line = reader.readLine();
+      while (line != null) {
+        if(line.contains(date) && line.contains("run"))  {
+          return true;
+        }
+        line = reader.readLine();
+      }
+      reader.close();
+    } catch ( IOException e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+
+  @Override
+  public int howManyHeartRateRecords(String date) {
+    int count = 0;
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader("data/Activity.data"));
+      String line = null;
+      line = reader.readLine();
+      while (line != null) {
+        if(line.contains(date))  {
+          JSONObject jsonObject = new JSONObject(line);
+          count += Integer.parseInt(jsonObject.get("bpm").toString());
+        }
+        line = reader.readLine();
+      }
+      reader.close();
+    } catch ( IOException e) {
+      e.printStackTrace();
+    }
+    return count;
+  }
 
   public static SearchResult search(String queryItem) {
     SearchResult searchResult = new SearchResult(SearchType.BRUTE_FORCE);
